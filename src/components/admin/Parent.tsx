456dfd432858981a24b8ff616e1a9f0976315928
isaacmatovu <matovuisaac646@gmail.com>
2025-06-11 13:33:13 +0300

    changed form layout

---
+++ src/components/admin/Parent.tsx
@@ -0,0 +1,26 @@
+import Button from "@/components/common/Button";
+import StudentStatistics from "./StudentStatistics";
+import Form from "../common/form";
+
+export default function Parent() {
+  return (
+    <div className="flex justify-between">
+      <div>
+        <div>
+          <h1 className="text-2xl text-gray-600">Parent</h1>
+          <p className="text-xl text-gray-600">
+            Manage Parent record and enrollment
+          </p>
+        </div>
+        <Form title="Parent" />
+      </div>
+      <div className="flex justify-start items-start gap-x-4 pt-14">
+        <Button>View Parent</Button>
+        <Button>Import Parents</Button>
+      </div>
+      <div className="pt-14">
+        <StudentStatistics studentStats="Parent Statistics" TotalStats={617} />
+      </div>
+    </div>
+  );
+}
