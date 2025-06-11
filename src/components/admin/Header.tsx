0807dc4f6a26b8cde0508c0e735f1b2e4d6009b4
isaacmatovu <matovuisaac646@gmail.com>
2025-06-11 13:17:35 +0300

    made header text mobile responsive

--- src/components/admin/Header.tsx
+++ src/components/admin/Header.tsx
@@ -1,13 +1,17 @@
 import { RiAdminFill } from "react-icons/ri";
+interface HeaderProps {
+  title: string;
+}
 
-export default function Header() {
+export default function Header(props: HeaderProps) {
+  const { title } = props;
   return (
     <div className="flex items-center justify-between bg-gray-100 p-6 border-b border-gray-400">
       <div className="flex items-center gap-x-4">
-        <h1 className="text-2xl">Admin</h1>
+        <h1 className="text-2xl">{title}</h1>
         <RiAdminFill className="text-3xl text-black" />
       </div>
-      <p className="text-2xl">School Management Sysytem</p>
+      <p className="text-sm sm:text-2xl">School Management Sysytem</p>
     </div>
   );
 }
