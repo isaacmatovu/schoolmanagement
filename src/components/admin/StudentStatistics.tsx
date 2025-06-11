adfed4d209080cfb526657a3c70f2f67a12db48d
isaacmatovu <matovuisaac646@gmail.com>
2025-06-11 13:15:59 +0300

    added student statistics

--- src/components/admin/StudentStatistics.tsx
+++ src/components/admin/StudentStatistics.tsx
@@ -1,15 +1,19 @@
 import { PiStudentFill } from "react-icons/pi";
+interface StudentProps {
+  studentStats: string;
+  TotalStats: number;
+}
 
-export default function StudentStatistics() {
+export default function StudentStatistics(props: StudentProps) {
+  const { studentStats, TotalStats } = props;
   return (
     <div className="w-full max-w-[400px] border border-gray-400 p-4 rounded-lg shadow-lg">
       <div className="flex justify-between bg-gray-200 border-b border-gray-400 p-4 rounded-lg">
-        <h1>Student Stastics</h1>
+        <h1>{studentStats}</h1>
         <PiStudentFill className="text-3xl text-blue-400" />
       </div>
       <div className="flex gap-x-3">
-        <p>Total Students</p>
-        <p>45</p>
+        <p>{TotalStats}</p>
       </div>
     </div>
   );
