b7c8063786ac835b2906c4b6e4e344e784db04e7
isaacmatovu <matovuisaac646@gmail.com>
2025-06-30 20:46:28 +0300

    removed student navbar

--- src/components/Students/navButton.tsx
+++ src/components/Students/navButton.tsx
@@ -1,15 +1,15 @@
-"use client";
+// "use client";
 
-import { useRouter } from "next/navigation";
-import Button from "../common/Button";
-interface ButtonProps {
-  id: number;
-}
-export default function NavButton(props: ButtonProps) {
-  const { id } = props;
-  const router = useRouter();
-  function handleClick() {
-    router.push(`/posts/${id}`);
-  }
-  return <Button onClick={handleClick} />;
-}
+// import { useRouter } from "next/navigation";
+// import Button from "../common/Button";
+// interface ButtonProps {
+//   id: number;
+// }
+// export default function NavButton(props: ButtonProps) {
+//   const { id } = props;
+//   const router = useRouter();
+//   function handleClick() {
+//     router.push(`/posts/${id}`);
+//   }
+//   return <Button onClick={handleClick} />;
+// }
