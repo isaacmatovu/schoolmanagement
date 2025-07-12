c518dd7720d5514912a01554ec1e74a29c297513
isaacmatovu <matovuisaac646@gmail.com>
2025-07-12 10:11:12 +0300

    added button type prop to button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -3,13 +3,14 @@ interface ButtonProps {
   children?: React.ReactNode;
   onClick?: () => void;
   type?: "button" | "submit" | "reset";
+  disabled?: boolean;
 }
 export default function Button(props: ButtonProps) {
   const { text, children, onClick, type = "button" } = props;
   return (
     <div className="flex justify-center items-center">
       <button
-        className="flex gap-x-2 bg-blue-500 hover:bg-blue-300 hover:text-black text-white px-4 py-2 rounded-lg shadow-2xl cursor-pointer"
+        className="flex disabled:bg-gray-400 gap-x-2 bg-blue-500 hover:bg-blue-300 hover:text-black text-white px-4 py-2 rounded-lg shadow-2xl cursor-pointer"
         onClick={onClick}
         type={type}
       >
